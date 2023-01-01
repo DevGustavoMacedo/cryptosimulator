72d63cdaea8359103c1bfcdbf114ca22b3242491
GustavoMacedoTech <121763412+GustavoMacedoTech@users.noreply.github.com>
2023-01-01 14:41:29 -0300

    feat: data coins

---
+++ src/data/coins.ts
@@ -0,0 +1,84 @@
+import { Coins } from "../types/Coins"
+
+const Coins: Coins = {
+  'bitcoin': {
+    name: 'Bitcoin',
+    symbol: 'BTC',
+    icon: '/btc.svg',
+    minDate: 1346457600000
+  },
+  'ethereum': {
+    name: 'Ethereum',
+    symbol: 'ETH',
+    icon: '/eth.svg',
+    minDate: 1459468800000
+  },
+  'litecoin': {
+    name: 'Litecoin',
+    symbol: 'LTC',
+    icon: '/ltc.svg',
+    minDate: 1491004800000
+  },
+  'binance-coin': {
+    name: 'Binance Coin',
+    symbol: 'BNB',
+    icon: '/bnb.svg',
+    minDate: 1517443200000
+  },
+  'solana': {
+    name: 'Solana',
+    symbol: 'SOL',
+    icon: '/sol.svg',
+    minDate: 1614556800000
+  },
+  'monero': {
+    name: 'Monero',
+    symbol: 'XMR',
+    icon: '/xmr.svg',
+    minDate: 1483228800000
+  },
+  'dash': {
+    name: 'Dash',
+    symbol: 'DASH',
+    icon: '/dash.svg',
+    minDate: 1470009600000
+  },
+  'polkadot': {
+    name: 'Polkadot',
+    symbol: 'DOT',
+    icon: '/dot.svg',
+    minDate: 1609459200000
+  },
+  'quant': {
+    name: 'Quant',
+    symbol: 'QNT',
+    icon: '/qnt.svg',
+    minDate: 1598918400000
+  },
+  'avalanche': {
+    name: 'Avalanche',
+    symbol: 'AVAX',
+    icon: '/avax.svg',
+    minDate: 1622505600000
+  },
+  'compound': {
+    name: 'Compound',
+    symbol: 'COMP',
+    icon: '/comp.svg',
+    minDate: 1593561600000
+  },
+  'kusama': {
+    name: 'Kusama',
+    symbol: 'KSM',
+    icon: '/ksm.svg',
+    minDate: 1598918400000
+  },
+  'decred': {
+    name: 'Decred',
+    symbol: 'DCR',
+    icon: '/dcr.svg',
+    minDate: 1491004800000
+  },
+}
+
+export default Coins
